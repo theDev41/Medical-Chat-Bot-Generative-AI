357674b03c3e95998b93e468cfcbdb44b8ebbb7c
Istiaq Ahmad <istiaqahmad41@gmail.com>
2025-04-16 10:32:13 +0600

    commmit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Istiaq Ahmad',
+    author_email= 'istiaqahmad41@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
